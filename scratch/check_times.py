31d80be7a348ca253617135bc289ea3ce67ba257
Kklosowiak <Konrad.klosowiak@gmail.com>
2026-05-06 21:37:54 -0400

    Cleanup workspace

--- scratch/check_times.py
+++
@@ -1,8 +0,0 @@
-import json
-from datetime import datetime
-
-with open('data/snapshot_20260426_124348.json', 'r') as f:
-    data = json.load(f)
-
-for game in data.get('odds', []):
-    print(f"{game['home_team']} vs {game['away_team']} | {game['commence_time']}")
